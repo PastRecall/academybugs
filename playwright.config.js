518e5b221b81b2db197d639088d7681cb74d4bb3
PastRecall <dy_lamba@bk.ru>
2024-12-02 19:07:12 +0300

    fifth step

--- playwright.config.js
+++ playwright.config.js
@@ -21,13 +21,10 @@ module.exports = defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: process.env.CI ? [['html', {open: 'never'}], ['line']] : [['html', {open: 'never'}], ['line']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    baseURL: process.env.BaseUrl ?? https://academybugs.com/find-bugs/#, 
     trace: 'on-first-retry',
   },
 
